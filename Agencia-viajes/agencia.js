84a9e7ee135257775e55c1fa1b3988e0bc11f6b3
cascodenotch <catalinaarciniegassaenz@gmail.com>
2024-10-25 16:36:34 +0200

    Añadidas funciones guardar entradas y filtrar

--- Agencia-viajes/agencia.js
+++ Agencia-viajes/agencia.js
@@ -1,19 +1,47 @@
+let arrayEntradas = [];
+let arrayEntradasFiltradas=[];
+
 function guardarEntrada(){
     
+    // recopilar los valores del formulario
+
     let nombre = document.getElementById("nombre").value
     let origen = document.getElementById("origen").value
     let destino = document.getElementById("destino").value
     let numPersonas = document.getElementById ("numPersonas").value
     let fecha = document.getElementById ("fecha").value
 
-   let entrada {
-        nombre: nombre,
-        origen: origen,
-        destino: destino,
-        personas: numPersonas,
-        fecha: fecha,
+    // crear un objeto con los valores del formulario
+
+    let entrada = {nombreEntrada: nombre,
+        origenEntrada: origen,
+        destinoEntrada: destino,
+        personasEntrada: numPersonas,
+        fechaEntrada: fecha,
     }
+    
+    // agregar los objetos al array 
+    
+    arrayEntradas.push(entrada);
+    console.log(arrayEntradas);
+    return arrayEntradas;
 
-    return entrada;
+}   
+
+// function mostrarEntradas (){
+
+//     for (let i=0; i<arrayEntradas.length; i++){
+    
+//         if(arrayEntradas[i].origenEntrada == "Canarias"){
+//          arrayEntradasFiltradas.push(arrayEntradas[i]);
+//         console.log(arrayEntradasFiltradas);
+//         }
+//     }
+// }
+
+function añadirSeccion (){
+
+let nuevoElemento = document.createElement("p");
+section.appendChild (nuevoElemento);
+    
 }
-
